69480db17a556a15f437d765efe408a48cef286e
Francisco Villar <francisco.jesus.villar@gmail.com>
2016-12-27 09:19:57 -0500

    Changed to a Json Array

--- authors.js
+++ authors.js
@@ -1,3 +1,3 @@
-{"Id":1,"firstName":"Cory","lastName":"House","Courses":null}
-{"Id":2,"firstName":"John","lastName":"Smith","Courses":null}
-{"Id":3,"firstName":"Randy","lastName":"White","Courses":null}
+[{"Id":1,"firstName":"Cory","lastName":"House","Courses":null},
+{"Id":2,"firstName":"John","lastName":"Smith","Courses":null},
+{"Id":3,"firstName":"Randy","lastName":"White","Courses":null}]
